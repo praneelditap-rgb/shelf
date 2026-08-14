1a588b795862e1da9a3f172b7a2e171ec6322ad5
praneelditap-rgb <praneel.dita.p@gmail.com>
2026-08-14 19:44:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SHELF_CONFIG = {
   SUPABASE_URL: "https://bakovsmnwuungfckgwpe.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_B4jG_V0To_...",
+  SUPABASE_ANON_KEY: "sb_publishable_B4jG_V0To_pPSbCnDEmz9Q_OwcT7L_G",
 
-  ALLOWED_EMAILS: ["your.email@gmail.com"],
+  ALLOWED_EMAILS: ["praneel.dita@gmail.com"],
 };
